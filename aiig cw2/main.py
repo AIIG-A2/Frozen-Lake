9c7a5de935e6b98d60583d59625bb9cbea6c68fa
your name <104041289+RuisiTX@users.noreply.github.com>
2023-01-04 19:26:28 +0000

    task 4&6

--- aiig cw2/main.py
+++ aiig cw2/main.py
@@ -5,6 +5,7 @@ from itertools import product
 
 from task1 import Environment
 
+
 # Configures numpy print options
 @contextlib.contextmanager
 def _printoptions(*args, **kwargs):
@@ -94,11 +95,11 @@ class FrozenLake(Environment):
 
         self.absorbing_state = n_states - 1
 
-        #TODO:
+        # TODO:
 
         Environment.__init__(self, n_states, n_actions, max_steps, pi, seed=seed)
-        #direction
-        self.actions=[(-1,0),(0,-1),(1,0),(0,1)]
+        # direction
+        self.actions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
         self.itos = list(product(range(self.lake.shape[0]), range(self.lake.shape[1])))
         self.stoi = {s: i for (i, s) in enumerate(self.itos)}
 
@@ -126,18 +127,17 @@ class FrozenLake(Environment):
         return state, reward, done
 
     def p(self, next_state, state, action):
-        return self._p[next_state,state,action]
+        return self._p[next_state, state, action]
 
-
-    #TODO:
+    # TODO:
 
     def r(self, next_state, state, action):
-        if state==15:
+        if state == 15:
             return 1
         else:
             return 0
 
-    #TODO:
+    # TODO:
 
     def render(self, policy=None, value=None):
         if policy is None:
@@ -183,7 +183,7 @@ def play(env):
 
 
 def policy_evaluation(env, policy, gamma, theta, max_iterations):
-    value = np.zeros(env.n_states, dtype=np.float)
+    value = np.zeros(env.n_states, dtype=float)
 
     for _ in range(max_iterations):
         delta = 0
@@ -258,43 +258,46 @@ def value_iteration(env, gamma, theta, max_iterations, value=None):
     print(index)
     return policy, value
 
+
 def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None, optimal_value=None, find_episodes=False):
     random_state = np.random.RandomState(seed)
     eta = np.linspace(eta, 0, max_episodes)
     epsilon = np.linspace(epsilon, 0, max_episodes)
     q = np.zeros((env.n_states, env.n_actions))
 
+    # Loop through the maximum number of episodes
     for i in range(max_episodes):
         s = env.reset()
+        # Set the terminal flag to False
         terminal = False
+        # Select an action using an e-greedy policy based on Q and the current epsilon value
         a = e_greedy(q[s], epsilon[i], env.n_actions, random_state)
 
         # Select action a for state s according to an e-greedy policy based on Q. by random
         while not terminal:
+            # Take action a and get the next state, reward, and terminal flag
             next_s, r, terminal = env.step(a)
+            # Select the next action using an e-greedy policy based on the Q-values for the next state
             next_a = e_greedy(q[next_s], epsilon[i], env.n_actions, random_state)
+            # Update the Q-value for state s and action a
             q[s][a] = q[s][a] + eta[i] * (r + (gamma * q[next_s][next_a]) - q[s][a])
             s = next_s
             a = next_a
 
+        # If find_episodes is True, check if the optimal policy has been found
         if find_episodes:
-            value_new = policy_evaluation(env, q.argmax(axis=1), gamma, theta=0.001, max_iterations=100)
+            # Evaluate the current policy using policy evaluation
+            value_new = policy_evaluation(env, q.argmax(axis=1), gamma, theta=0.001, max_iterations=128)
+            # Check if the optimal policy has been found by comparing the values to the optimal value
             if all(abs(optimal_value[i] - value_new[i]) < 0.001 for i in range(len(value_new))):
-                print('Episodes to find the optimal policy: ' + str(i))
+                print('Episodes to find the optimal policy: {}' + format(i))
                 return q.argmax(axis=1), value_new
 
+    # If the optimal policy was not found, return the policy and value function for the final iteration
     policy = q.argmax(axis=1)
     value = q.max(axis=1)
 
     return policy, value
-
-def e_greedy(q, epsilon, n_actions, random_state):
-    if random.uniform(0, 1) < epsilon:
-        a = random_state.choice(np.flatnonzero(q == q.max()))
-    else:
-        a = random_state.randint(n_actions)
-    return a
-
 
 
 def q_learning(env, max_episodes, eta, gamma, epsilon, seed=None, optimal_value=None, find_episodes=False):
@@ -313,15 +316,132 @@ def q_learning(env, max_episodes, eta, gamma, epsilon, seed=None, optimal_value=
             s = next_s
 
         if find_episodes:
-            value_new = policy_evaluation(env, q.argmax(axis=1), gamma, theta=0.001, max_iterations=100)
-            if all(abs(optimal_value[i]-value_new[i]) < 0.001 for i in range(len(value_new))):
-                print('Episodes to find the optimal policy: ' + str(i))
+            value_new = policy_evaluation(env, q.argmax(axis=1), gamma, theta=0.001, max_iterations=128)
+            if all(abs(optimal_value[i] - value_new[i]) < 0.001 for i in range(len(value_new))):
+                print('Episodes to find the optimal policy: ' + format(i))
                 return q.argmax(axis=1), value_new
 
     policy = q.argmax(axis=1)
     value = q.max(axis=1)
 
     return policy, value
+
+
+################ Non-tabular model-free algorithms ################
+
+class LinearWrapper:
+    def __init__(self, env):
+        # Save a reference to the environment
+        self.env = env
+
+        # Save the number of actions and states in the environment
+        self.n_actions = self.env.n_actions
+        self.n_states = self.env.n_states
+        # Calculate the number of features based on the number of actions and states
+        self.n_features = self.n_actions * self.n_states
+
+    def encode_state(self, s):
+        # Initialize a matrix of zeros with the same number of rows
+        # as actions and the same number of columns as features
+        features = np.zeros((self.n_actions, self.n_features))
+        for a in range(self.n_actions):
+            # Calculate the index for the current state and action
+            i = np.ravel_multi_index((s, a), (self.n_states, self.n_actions))
+            # Set the corresponding element in the features matrix to 1.0
+            features[a, i] = 1.0
+
+        return features
+
+    def decode_policy(self, theta):
+        # Initialize the policy and value arrays
+        policy = np.zeros(self.env.n_states, dtype=int)
+        value = np.zeros(self.env.n_states)
+
+        for s in range(self.n_states):
+            # Encode the current state
+            features = self.encode_state(s)
+            # Calculate the Q-values for the current state using the current theta
+            q = features.dot(theta)
+
+            # Set the policy for the current state to the action with the highest Q-value
+            policy[s] = np.argmax(q)
+            # Set the value for the current state to the highest Q-value
+            value[s] = np.max(q)
+
+        return policy, value
+
+    def reset(self):
+        return self.encode_state(self.env.reset())
+
+    def step(self, action):
+        state, reward, done = self.env.step(action)
+
+        return self.encode_state(state), reward, done
+
+    def render(self, policy=None, value=None):
+        self.env.render(policy, value)
+
+
+def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
+    random_state = np.random.RandomState(seed)
+
+    eta = np.linspace(eta, 0, max_episodes)
+    epsilon = np.linspace(epsilon, 0, max_episodes)
+
+    theta = np.zeros(env.n_features)
+
+    for i in range(max_episodes):
+
+        features = env.reset()
+        q = features.dot(theta)
+        # Select an action using an e-greedy policy based on the Q-values and the current epsilon value
+        a = e_greedy(q, epsilon[i], env.n_actions, random_state)
+        # Set the terminal flag to False
+        terminal = False
+        while not terminal:
+            # Take action a and get the next state, reward, and terminal flag
+            next_s, r, terminal = env.step(a)
+            # Calculate the temporal difference error
+            delta = r - q[a]
+            # Calculate the Q-values for the next state using the current theta
+            q = next_s.dot(theta)
+            # Select the next action using an e-greedy policy based on the Q-values for the next state
+            a_new = e_greedy(q, epsilon[i], env.n_actions, random_state)
+
+            # Update the temporal difference error
+            delta = delta + (gamma * max(q))
+            # Update theta using the temporal difference error
+            theta = theta + eta[i] * delta * features[a]
+            # Set the current features to the next state
+            features = next_s
+            # Set the current action to the next action
+            a = a_new
+
+    return theta
+
+
+def linear_q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
+    random_state = np.random.RandomState(seed)
+
+    eta = np.linspace(eta, 0, max_episodes)
+    epsilon = np.linspace(epsilon, 0, max_episodes)
+
+    theta = np.zeros(env.n_features)
+
+    for i in range(max_episodes):
+        features = env.reset()
+        q = features.dot(theta)
+        terminal = False
+
+        while not terminal:
+            a = e_greedy(q, epsilon[i], env.n_actions, random_state)
+            next_s, r, terminal = env.step(a)
+            delta = r - q[a]
+            q = next_s.dot(theta)
+            delta = delta + (gamma * max(q))
+            theta = theta + (eta[i] * delta * features[a])
+            features = next_s
+    return theta
 
 
 def e_greedy(q, epsilon, n_actions, random_state):
@@ -331,19 +451,20 @@ def e_greedy(q, epsilon, n_actions, random_state):
         a = random_state.randint(n_actions)
     return a
 
+
 def main():
-    seed=0;
+    seed = 0;
     # Small lake
     lake = [['&', '.', '.', '.'],
             ['.', '#', '.', '#'],
             ['.', '.', '.', '#'],
             ['#', '.', '.', '$']]
-    env=FrozenLake(lake,slip=0.1,max_steps=16,seed=seed)
-    play(env)
+    env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
+    # play(env)
     print('# Model-based algorithms')
     gamma = 0.9
     theta = 0.001
-    max_iterations = 100
+    max_iterations = 128
 
     print('')
 
@@ -362,9 +483,9 @@ def main():
     print('')
 
     print('# Model-free algorithms')
-    max_episodes = 40000
-    eta = 0.1
-    epsilon = 0.9
+    max_episodes = 20000
+    eta = 0.5
+    epsilon = 0.5
 
     print('')
 
@@ -374,16 +495,30 @@ def main():
     env.render(policy, value)
 
     print('')
-    print(opt_value)
-    print(policy_evaluation(env, policy, gamma, theta=0.001, max_iterations=100))
 
     print('## Q-learning')
     policy, value = q_learning(env, max_episodes, eta, gamma, epsilon, seed=seed, optimal_value=opt_value,
                                find_episodes=False)
     env.render(policy, value)
 
+    linear_env = LinearWrapper(env)
+
+    print('## Linear Sarsa')
+
+    parameters = linear_sarsa(linear_env, max_episodes, eta,
+                              gamma, epsilon, seed=seed)
+    policy, value = linear_env.decode_policy(parameters)
+    linear_env.render(policy, value)
+
+    print('')
+
+    print('## Linear Q-learning')
+
+    parameters = linear_q_learning(linear_env, max_episodes, eta,
+                                   gamma, epsilon, seed=seed)
+    policy, value = linear_env.decode_policy(parameters)
+    linear_env.render(policy, value)
+
 
 if __name__ == "__main__":
     main()
-
-
