6ad69aea2abc25dc4a7ab47504fdace90600f241
your name <104041289+RuisiTX@users.noreply.github.com>
2023-01-08 17:25:17 +0000

    final version

--- group[I]/environment.py
+++ group[I]/environment.py
@@ -1,23 +1,36 @@
 import numpy as np
 
 
-
 class EnvironmentModel:
     def __init__(self, n_states, n_actions, seed=None):
         self.n_states = n_states
         self.n_actions = n_actions
         self.random_state = np.random.RandomState(seed)
-#  The method p returns the probability of transitioning
-# from state to next state given action
+
     def p(self, next_state, state, action):
+        """
+        The method p returns the probability of transitioning
+        from state to next state given action
+
+        """
         raise NotImplementedError()
-#  The method r returns the expected reward in having transitioned
-# from state to next state given action.
+
+    #  The method r returns the expected reward in having transitioned
+    # from state to next state given action.
     def r(self, next_state, state, action):
+        """
+        The method r returns the expected reward in having transitioned
+        from state to next state given action.
+
+        """
         raise NotImplementedError()
-# The method draw receives a pair of state and action and returns a
-# state drawn according to p together with the corresponding expected reward
+
     def draw(self, state, action):
+        """
+        The method draw receives a pair of state and action and returns a
+        state drawn according to p together with the corresponding expected reward
+
+        """
         p = [self.p(ns, state, action) for ns in range(self.n_states)]
         next_state = self.random_state.choice(self.n_states, p=p)
         reward = self.r(next_state, state, action)
@@ -51,5 +64,6 @@ class Environment(EnvironmentModel):
         self.state, reward = self.draw(self.state, action)
 
         return self.state, reward, done
-    def render(self,policy=None,value=None):
+
+    def render(self, policy=None, value=None):
         raise NotImplementedError()
